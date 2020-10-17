9cb9d2da20b6dd92a7090444d0e14cb895081f96
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-17 12:33:48 -0600

    Import libraries

--- natural_language_processing.py
+++ natural_language_processing.py
@@ -1,7 +1,9 @@
 # Natural Language Processing tutorial from Machine Learning A-Z - SuperDataScience -> Input by Ryan L Buchanan 16OCT20
 
 # Import libraries
-
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
 
 # Import dataset
 
