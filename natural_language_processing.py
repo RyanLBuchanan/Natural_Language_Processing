041f17036a1f8550ee7a75b82b42d85af86684b1
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-17 12:30:49 -0600

    Set up project in Spyder

--- natural_language_processing.py
+++ natural_language_processing.py
@@ -1,2 +1,26 @@
 # Natural Language Processing tutorial from Machine Learning A-Z - SuperDataScience -> Input by Ryan L Buchanan 16OCT20
 
+# Import libraries
+
+
+# Import dataset
+
+
+# Clean the texts
+
+
+# Create the Bag of Words model
+
+
+# Split the dataset into the Training set and the Test set
+
+
+# Train the Naive Bayes Model on the Training set
+
+
+# Predict the Test set results
+
+
+# Make the Confusion Matrix
+
+ 
