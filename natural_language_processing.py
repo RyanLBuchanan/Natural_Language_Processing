a0b4a77db89ac7e6f60e22805d7ef4ac892a3108
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-19 16:06:27 -0600

    Printed Confusion Matrix & accuracy Spyder

--- natural_language_processing.py
+++ natural_language_processing.py
@@ -49,11 +49,14 @@ from sklearn.naive_bayes import GaussianNB
 classifier = GaussianNB()
 classifier.fit(X_train, y_train)
 
-
-
 # Predict the Test set results
-
+y_pred = classifier.predict(X_test)
+print(np.concatenate((y_pred.reshape(len(y_pred), 1), y_test.reshape(len(y_test), 1)),1))
 
 # Make the Confusion Matrix
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm = confusion_matrix(y_test, y_pred)
+print(cm)
+accuracy_score(y_test, y_pred)
 
  
