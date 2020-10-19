c21d1f1faa910fdc6e9eb4be5fa2546c20c36b96
Ryan L Buchanan <buchanan.ryan22@gmail.com>
2020-10-19 15:57:37 -0600

    Split Training & Test sets

--- natural_language_processing.py
+++ natural_language_processing.py
@@ -41,7 +41,8 @@ y = dataset.iloc[:, -1].values
 len(X[0])
 
 # Split the dataset into the Training set and the Test set
-
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state=0)
 
 # Train the Naive Bayes model on the Training set
 
